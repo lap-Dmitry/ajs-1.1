85de06dc11c015f51197f72bd1486a29f3314ba9
Dmitry Lapshin <lapshin.dm56@gmail.com>
2021-07-26 12:18:53 +0300

    fixed one

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
 const characters = [
   { name: 'мечник', health: 10 },
   { name: 'маг', health: 100 },
   { name: 'маг', health: 0 },
   { name: 'лучник', health: 0 },
 ];
-
+// eslint-disable-next-line no-unused-vars
 const alive = characters.filter((item) => item.health > 0);
-const fix = (data) => data;
-fix(alive);
